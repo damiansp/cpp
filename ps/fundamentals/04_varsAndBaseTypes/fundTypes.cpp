755f6ee5db1a6de1ab49058971effa9fa4a836c3
Damian Satterthwaite-Phillips <dsatterthwaite@cbtnuggets.com>
2021-09-21 06:15:09 -0700

    +fund types, init ignore

---
+++ ps/fundamentals/04_varsAndBaseTypes/fundTypes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+int main() {
+  int i1 = 1;
+  int i2;
+  i2 = 3;
+  int i3(3);
+  //int i4{4}; // C++ 20+
+  double d1 = 2.2;
+  double d2 = i1;
+  int i5 = d1; // 2
+  char c1 = 'a';
+  bool flag = false;
+  flag = i1;   // true
+
+  auto a1 = 1;
+  auto a2 = 2.2;
+  auto a3 = 'c';
+  auto a4 = "s";
+  auto a5 = true;
+  auto a6 = 3L;
+  //auto a7 = 1'000'000'000'000; // C++ 14+
+  auto a7 = 1000000000000;
+  auto a8 = 0xFF;  // 255
+  auto a9 = 0b111; // 7
+
+  return 0;
+}
