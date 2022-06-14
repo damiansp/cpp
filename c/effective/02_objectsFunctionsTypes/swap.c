6bca56b06be8744af35c0f09566119f4df47530f
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-06-14 11:08:28 -0700

    effective c: +swap

---
+++ c/effective/02_objectsFunctionsTypes/swap.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+void swap(int a, int b) {
+  int temp = a;
+  a = b;
+  b = temp;
+  // a: 17, b:21 (swapped for duration of func)
+  printf("swap: a = %d, b = %d\n", a, b);
+}
+
+
+void swap_in_place(int* pa, int* pb) {
+  int temp = *pa;
+  *pa = *pb;
+  *pb = temp;
+}
+
+int main() {
+  int a = 21;
+  int b = 17;
+  swap(a, b);
+  printf("main: a = %d, b = %d\n", a, b);  // a: 21, b: 17 (not swapped)
+  swap_in_place(&a, &b);
+  printf("main: a = %d, b = %d\n", a, b);  // a: 17, b: 21 (swapped)
+  return 0;
+}
