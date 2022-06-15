a453156d94f1281417d1e8826b08deebeec7f564
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-06-15 07:25:21 -0700

    effective/+alignas ex

---
+++ c/effective/02_objectsFunctionsTypes/align_as.c
@@ -0,0 +1,14 @@
+struct S {
+  int i;
+  double d;
+  char c;
+};
+
+
+int main() {
+  unsigned char bad_buff[sizeof(struct S)];
+  struct S* bad_s_ptr = (struct S*) bad_buff; // wrong pointer alignment
+
+  _Alignas(struct S) unsigned char good_buff[sizeof(struct S)];
+  struct S* good_s_ptr = (struct S*) good_buff; // correct
+}
