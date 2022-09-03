182a4039638302b232d331561f136407f7731b31
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-09-03 11:41:04 -0700

    c/eff/ init 6 memory alloc w/ malloc

---
+++ c/effective/06_dynamic_memory_allocation/malloc.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+
+typedef struct {
+  char c[10];
+  int i;
+  double d;
+} widget;
+
+
+int main(void) {
+  widget *pw = malloc(sizeof(widget));
+  if (p == NULL) { /* handle allocation err */; }
+  // ...
+}
