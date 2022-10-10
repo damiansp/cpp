e3bc452eedb43eb04508a4d3ed17128ae132737e
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2022-10-09 17:01:20 -0700

    +iterative quadratic

---
+++ c/numerical_applied/02_solving_equations/iterative_quadratic.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <math.h>
+#include <stdio.h>
+
+/* Solve x^2 - 8x - 13 = 0 by iterative trial and error */
+int main() {
+  // curve should cross x-axis
+  float p1 = -8.,    // coef for linear term
+    p2 = 1.,         // coef for squared term
+    const_val = -13, // const term
+    lower,           // value of x where curve below x-axis
+    upper;           // above x-axis
+  double test_low, test_high, test_val, middle;
+  int i, iterations;
+
+  printf("Enter initial lower value: ");
+  scanf("%f", &lower);
+  printf("And initial upper: ");
+  scanf("%f", &upper);
+  printf("No. of iterations: ");
+  scanf("%d", &iterations);
+  test_low = lower;
+  test_high = upper;
+  printf("Equation is : %fx^2 %fx %f = 0\n", p2, p1, const_val);
+  for (i = 0; i < iterations; i++) {
+    middle = (test_high + test_low) / 2;
+    printf("  x appx: %f\n", middle);
+    test_val = p2*pow(middle, 2) + p1*middle + const_val;
+    if (test_val == 0) {
+      printf("x = %f\n", middle);
+      return 0;
+    }
+    if (test_val > 0) {
+      test_high = middle;
+    } else {
+      test_low = middle;
+    }
+  }
+  printf("x = %f\n", middle);
+  
+  return 0;
+}
