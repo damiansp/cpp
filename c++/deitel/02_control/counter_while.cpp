4f0ed222cb77567dd048809c6506d99b80e26b2d
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-04-28 16:13:13 -0700

    +counterwhile, for

---
+++ c++/deitel/02_control/counter_while.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+int main() {
+  int i = 1;
+  while (i <= 10) {
+    cout << i++ << endl;
+  }
+  return 0;
+}
---
+++ c++/deitel/02_control/for.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+
+int main() {
+  for (int i = 1; i <= 10; i++) {
+    cout << i << endl;
+  }
+  return 0;
+}
