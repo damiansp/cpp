b3588e2f556771b689ff9bb8133bcc8f22734fb2
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-10-02 07:47:08 -0700

    fin rational

--- c++/deitel/06_classes_data_abstraction/exercises/rational/rational.cpp
+++ c++/deitel/06_classes_data_abstraction/exercises/rational/rational.cpp
@@ -19,10 +19,27 @@ Rational Rational::add(Rational other) {
   int n1 = numerator,
     n2 = other.numerator,
     d = denominator;
+
   if (denominator != other.denominator) {
     n1 = numerator * other.denominator;
     n2 = other.numerator * denominator;
     d = denominator * other.denominator;
   }
   return Rational(n1 + n2, d);
+}
+
+
+Rational Rational::subtract(Rational other) {
+  return add(Rational(-other.numerator, other.denominator));
+}
+
+
+Rational Rational::multiply(Rational other) {
+  return Rational(
+    numerator * other.numerator, denominator * other.denominator);
+}
+
+
+Rational Rational::divide(Rational other) {
+  return multiply(Rational(other.denominator, other.numerator));
 }
--- c++/deitel/06_classes_data_abstraction/exercises/rational/rational_driver.cpp
+++ c++/deitel/06_classes_data_abstraction/exercises/rational/rational_driver.cpp
@@ -8,12 +8,31 @@ int main() {
   Rational r1 = Rational(1, 2);
   Rational r2 = Rational(2, 3);
   Rational sum = r1.add(r2);
+  Rational prod = r1.multiply(r2);
 
   r1.print();
   cout << " + ";
   r2.print();
   cout << " = ";
   sum.print();
+  cout << endl;
+  sum.print();
+  cout << " - ";
+  r1.print();
+  cout << " = ";
+  sum.subtract(r1).print();
+  cout << endl;
+  r1.print();
+  cout << "(";
+  r2.print();
+  cout << ") = ";
+  prod.print();
+  cout << "\n(";
+  prod.print();
+  cout << ") / (";
+  r1.print();
+  cout << ") = ";
+  prod.divide(r1).print();
   cout << endl;
   return 0;
 }
