46ef464f0f34013e6999403eac3fcde92a025c28
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-10-20 21:54:23 -0700

    7/+time

---
+++ c++/deitel/07_classes2/time/time.h
@@ -0,0 +1,25 @@
+#ifndef TIME_H
+#define TIME_H
+
+
+class Time {
+ public:
+  Time(int=0, int=0, int=0);  // constructor
+  void set_time(int, int, int);  // h, m, s
+  void set_hour(int);
+  void set_minute(int);
+  void set_second(int);
+  int get_hour();
+  int get_minute();
+  int get_second();
+  void print();
+  void print_military();
+
+ private:
+  int hour;
+  int minute;
+  int second;
+};
+
+
+#endif
---
+++ c++/deitel/07_classes2/time/time_driver.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+#include "time.h"
+
+
+int main() {
+  Time wake_up(6, 45, 0);
+  const Time NOON(12, 0, 0);
+}
