91c4d97e2f470b991d3d267fdd4553ec40c66d2e
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2023-10-23 15:41:06 -0700

    +incr

---
+++ c++/deitel/07_classes2/incr/increment.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+class Increment {
+public:
+  Increment(int c=0, int i=1);
+  void add_increment() { count += increment; }
+  void print() const;
+
+private:
+  int count;
+  const int increment;
+};
+
+
+Increment::Increment(int c, int i): increment(i) /* init const member */ {
+  count = c;
+}
+
+
+void Increment::print() const {
+  cout << "count: " << count << ", increment: " << increment << endl;
+}
+
+
+int main() {
+  Increment value(10, 5);
+
+  cout << "Before incrementing: ";
+  value.print();
+  for (int i = 0; i < 3; i++) {
+    value.add_increment();
+    cout << "After increment " << i + 1 << ": ";
+    value.print();
+  }
+  return 0;
+}
