715d9f6da9902269dea33bc1874bc9e6e5a7e2fe
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-07-12 23:49:23 -0700

    init complex.h

---
+++ c++/deitel/08_operator_overloading/complex/complex.h
@@ -0,0 +1,18 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+
+
+class Complex {
+ public:
+  Complex(double=0., double=0.);
+  Complex operator+(const Complex&) const;
+  Complex operator-(const Complex&) const;
+  const Complex& operator=(const Complex&);
+  void print() const;
+
+ private:
+  double real;
+  double imaginary;
+};
+
+#endif
