4ec35b3bd74e3a1ab81f8ea2e572ab6ec15607c1
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2026-04-07 09:21:39 -0700

    fin ch 1

---
+++ c++/cpp23/01_basic_ideas/ex1_name_age.cpp
@@ -0,0 +1,11 @@
+// A complete C++ program
+//import std;
+#include <print>
+
+
+int main() {
+  int age {49};
+
+  std::println("Hello there. {} years old, eh? Who's a big boy?", age);
+  return 0;
+}
---
+++ c++/cpp23/README.md
@@ -0,0 +1,5 @@
+To compile:
+
+```
+>  g++ -std=c++23 my_file.cpp -o my_executable
+```
