e371cd20fb9ff10177d5750f7784cfda143c088f
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2026-06-20 22:25:21 -0700

    +month name

---
+++ c/kr/05_pointers_and_arrays/month_name.c
@@ -0,0 +1,8 @@
+/* return the name of the nth month */
+char* month_name(int n) {
+  static char* name[] = {
+    "Illegal month",
+    "January", "February", "March", "April", "May", "June", "July", "August",
+    "September", "October", "November", "December"};
+  return (n < 1 || n > 12) ? name[0] : name[n];
+}
