e6c56335eefae17ab63205ff9e63da694db9a3fc
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2026-07-12 16:51:25 -0700

    +sortlines

---
+++ c/kr/05_pointers_and_arrays/sortlines.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+
+
+char* lineptr[MAXLINES];  // pointers to text lines
+
+int readlines(char* lineptr[], int nlines);
+void writelines(char* lineptr[], int nlines);
+void qsort(void* lineptr[], int left, int right, int (*comp)(void*, void*));
+int numpcmp(char*, char*);
+
+
+/* sort input lines */
+int main(int argc, char* argv[]) {
+  int nlines;       // n input lines read
+  int numeric = 0;  // 1 if numeric sort
+
+  if (argc > 1 & strcmp(argv[1], "-n") == 0) { numeric = 1; }
+  if ((nlines = readlines(lineptr, MAXLINES)) > = 0) {
+    qsort(
+      (void**) lineptr,
+      0,
+      nlines - 1,
+      (int (*)(void*, void*))(numeric ? numcmp : strcmp));
+    writelines(lineptr, nlines);
+    return 0;
+  } else {
+    printf("Input too big to sort\n");
+    return 1;
+  }
+}
